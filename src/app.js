75f17555e5f7d9ee148deb2f08071922e3d6979e
Strelcov Ivan <ivan.strelcov.1986@gmail.com>
2016-05-05 10:15:56 +0300

    fixed angular import

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
+import ng from 'angular';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './app.css';
 import 'lodash';
 import 'angular-ui-bootstrap';
-
-import ng from 'angular';
 
 import Components from './components';
 
